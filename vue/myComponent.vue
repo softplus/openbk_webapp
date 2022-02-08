5c797668cfc34f7ca0f3f557ce5f928ec785e51c
btsimonh <simon@yellaumbrella.tv>
2022-02-08 20:16:42 +0000

    add tabs

--- vue/myComponent.vue
+++ vue/myComponent.vue
@@ -1,7 +1,29 @@
 <template>
-    <div style="height:80%; overflow:scroll;" id="logs"><pre>{{logs}}</pre></div>
-  <span class="example">Hello {{ msg }}</span>
+    <div class="fullscreen">
+    <!-- Tab links -->
+    <div class="tab">
+      <button class="tablinks" @click="open($event, 'Logs')">Logs</button>
+      <button class="tablinks" @click="open($event, 'Config')">Config</button>
+      <button class="tablinks" @click="open($event, 'Status')">Status</button>
+    </div>
+    
+    <!-- Tab content -->
+    <div class="tabcontent" v-if="tab === 'Logs'">
+      <div style="height:80%; overflow:scroll;" id="logs"><pre>{{logs}}</pre></div>
+    </div>
+    
+    <div class="tabcontent" v-if="tab === 'Config'">
+      <h3>Config</h3>
+      <p>Config page.</p>
+    </div>
+    
+    <div class="tabcontent" v-if="tab === 'Status'">
+      <h3>Status</h3>
+      <p>Status Page.</p>
+    </div>    
+    </div>
 </template>
+
 <script>
   export default {
     data () {
@@ -9,22 +31,32 @@
         msg: 'world!',
         color: 'blue',
         logs: '',
+        tab: 'Config',
       }
     },
     methods:{
+        open(evt, name) {
+            console.log('select tab '+name);
+            this.tab = name;
+        },       
+        
         showlogs(){
-          let url = window.device+'/lograw';
-          fetch(url)
-            .then(response => response.text())
-            .then(data => {
-                this.logs += data;
-                let splt = this.logs.split('\n');
-                while (splt.length > 400){
-                    splt.shift();
-                }
-                this.logs = splt.join('\n');
+            if (this.tab === 'Logs'){
+              let url = window.device+'/lograw';
+              fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    this.logs += data;
+                    let splt = this.logs.split('\n');
+                    while (splt.length > 400){
+                        splt.shift();
+                    }
+                    this.logs = splt.join('\n');
+                    setTimeout(()=>{this.showlogs();}, 1000);
+                });
+            } else {
                 setTimeout(()=>{this.showlogs();}, 1000);
-            });
+            }
         },
     },
     mounted (){
@@ -36,7 +68,47 @@
 </script>
 
 <style scoped>
-  .example {
-    color: v-bind('color');
-  }
+    .fullscreen{
+        height:100%;
+    }
+
+    .example {
+        color: v-bind('color');
+    }
+
+    /* Style the tab */
+    .tab {
+      overflow: hidden;
+      border: 1px solid #ccc;
+      background-color: #f1f1f1;
+    }
+    
+    /* Style the buttons that are used to open the tab content */
+    .tab button {
+      background-color: inherit;
+      float: left;
+      border: none;
+      outline: none;
+      cursor: pointer;
+      padding: 14px 16px;
+      transition: 0.3s;
+    }
+    
+    /* Change background color of buttons on hover */
+    .tab button:hover {
+      background-color: #ddd;
+    }
+    
+    /* Create an active/current tablink class */
+    .tab button.active {
+      background-color: #ccc;
+    }
+    
+    /* Style the tab content */
+    .tabcontent {
+      padding: 6px 12px;
+      border: 1px solid #ccc;
+      border-top: none;
+      height:90%;
+    }
 </style>
