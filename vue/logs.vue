2a9c6d66d1bcd693bde5aad3fce191d369259651
btsimonh <simon@yellaumbrella.tv>
2022-03-06 12:56:29 +0000

    add checkboxes per feature in logs

--- vue/logs.vue
+++ vue/logs.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="fullheight">
       <div ref="logs" style="height:80%; overflow:scroll;" id="logs"><pre>{{logs}}</pre></div>
+      <label for="pause">Pause</label><input type="checkbox" id="pause" name="pause" v-model="paused">
+      <button @click="clear">Clear</button>
+
+      <label :for="item" v-for="(item, index) in logfeaturenames" :key="item">{{item}}
+        <input type="checkbox" id="pause" name="pause" v-model="logfeatures[index]" @click="setfeature(index, this.checked)">
+      </label>
+
+      <label for="pause">Pause</label><input type="checkbox" id="pause" name="pause" v-model="paused">
       <form @submit="send($event)">
         <input v-model="cmd" id="obkcommand" type="text" autocomplete="on" style="width:60%;">
         <input type="submit">
@@ -14,9 +22,37 @@
       return {
         logs: '',
         cmd: '',
+        paused:false,
+        logfeaturenames:[
+            "HTTP:",//            = 0,
+            "MQTT:",//            = 1,
+            "CFG:",//             = 2,
+            "HTTP_CLIENT:",//     = 3,
+            "OTA:",//             = 4,
+            "PINS:",//            = 5,
+            "MAIN:",//            = 6,
+            "GEN:", //              = 7
+            "API:", // = 8
+            "LFS:", // = 9
+            "CMD:", // = 10
+        ],
+        logfeatures:[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
       }
     },
     methods:{
+        setfeature(index, enable){
+            let url = window.device+'/api/cmnd';
+            let cmnd = 'logfeature '+index+' '+(enable?'1':'0');
+            fetch(url, {body:cmnd, method:'POST'})
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+            })
+            .catch((e)=>{
+                console.error(e);
+            })
+        },
+
         send(ev){
             ev.preventDefault();
             let url = window.device+'/api/cmnd';
@@ -32,31 +68,46 @@
             })
         },
 
+        clear(){
+            this.logs = '';
+        },
+
         showlogs(){
             let url = window.device+'/lograw';
-            fetch(url)
-            .then(response => response.text())
-            .then(data => {
-                console.log(data);
-                this.logs += data;
-                let splt = this.logs.split('\n');
-                while (splt.length > 400){
-                    splt.shift();
-                }
-
-                if (this.$refs['logs']){
-                    let scrollTop = this.$refs['logs'].scrollTop;
-                    let scrollHeight = this.$refs['logs'].scrollHeight;
-                    let clientHeight = this.$refs['logs'].clientHeight;
-                    if (scrollTop + clientHeight >= scrollHeight - 10){
-                        setTimeout(()=>{this.$refs['logs'].scrollTop = scrollHeight + 1000});
+            if (!this.paused){
+                fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    data.replace(/\r/g, '');
+                    this.logs += data;
+                    let splt = this.logs.split('\n');
+                    // remove empty
+                    for (let i = splt.length-1; i >= 0; i--) {
+                        if (!splt[i]){
+                            splt.splice(i, 1);
+                        }
                     }
-                }
-                this.logs = splt.join('\n');
+
+                    while (splt.length > 400){
+                        splt.shift();
+                    }
+
+                    if (this.$refs['logs']){
+                        let scrollTop = this.$refs['logs'].scrollTop;
+                        let scrollHeight = this.$refs['logs'].scrollHeight;
+                        let clientHeight = this.$refs['logs'].clientHeight;
+                        if (scrollTop + clientHeight >= scrollHeight - 10){
+                            setTimeout(()=>{this.$refs['logs'].scrollTop = scrollHeight + 1000});
+                        }
+                    }
+                    this.logs = splt.join('\n');
+                    this.logtimer = setTimeout(()=>{this.showlogs();}, 500);
+                }).catch(()=>{
+                    this.logtimer = setTimeout(()=>{this.showlogs();}, 500);
+                });
+            } else {
                 this.logtimer = setTimeout(()=>{this.showlogs();}, 500);
-            }).catch(()=>{
-                this.logtimer = setTimeout(()=>{this.showlogs();}, 500);
-            });
+            }
         },
     },
     mounted (){
