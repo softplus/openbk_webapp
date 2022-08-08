fa3ab9d76a7f43740becdf14aeb315b12276f7c5
Indu Prakash <leolite1@gmail.com>
2022-08-08 06:29:06 -0500

    Adjusted rendering

--- vue/info.vue
+++ vue/info.vue
@@ -11,16 +11,51 @@
         <p>Chipset: {{chipset}}</p>
         <p v-if="error">Error: {{error}}</p>
       </div>
+
       <div class="right">
-        <p>Pin Settings:</p>
-        <div v-for="(role, index) in pins.roles" :key="index">
-          <span>{{index}}</span>
-          <select v-model="pins.roles[index]">
-            <option v-for="(name, index2) in pins.rolenames" :value="index2" :key="index2" :selected="(role == index2)">{{name}}</option>
-          </select>
-          <input type="number" min="0" max="32" step="1" v-model="pins.channels[index]">
-        </div>
-        <button @click="savePins">Save Pins</button>
+        <table width="100%">
+          <tr>
+            <td style="vertical-align:top; padding-right:5px; width: 250px;">
+              <p>Devices:</p>
+
+              <select v-model="selectedDevIndex">
+                <option v-for="(dev, index) in devices" :value="index" :key="index">{{dev.name}}</option>
+              </select>
+
+              <div v-if="selectedDevIndex>0">
+                {{ devices[selectedDevIndex].chip}} {{ devices[selectedDevIndex].board }}
+
+                <div v-if="devices[selectedDevIndex].urls">
+                  More Info: 
+                  <span v-for="(url,index) in devices[selectedDevIndex].urls" :key="url">
+                    <a v-bind:href="url" target="_blank">{{index+1}}</a>
+                  </span>
+                </div>
+
+                <ul>
+                  <li v-for="(pin,key) in devices[selectedDevIndex].pins" :key="key">
+                  Pin {{key}} as {{ pin.split(';')[0] }} on channel {{ pin.split(';')[1] }}
+                  </li>
+                </ul>
+                <button @click="useDevice">Use device</button>
+              </div>
+              
+            </td>
+            <td style="vertical-align:top; padding-left:5px;">
+
+              <p>Pin Settings:</p>
+              <div v-for="(role, index) in pins.roles" :key="index">
+                <span>{{index}}</span>
+                <select v-model="pins.roles[index]">
+                  <option v-for="(name, index2) in pins.rolenames" :value="index2" :key="index2" :selected="(role == index2)">{{name}}</option>
+                </select>
+                <input type="number" min="0" max="32" step="1" v-model="pins.channels[index]">
+              </div>
+              <button @click="savePins">Save Pins</button>
+              
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
 </template>
@@ -43,9 +78,39 @@
 
         error:'',
         interval: null,
+
+        devices: null,
+        selectedDevIndex: 0,
+        pinRoleNames: [
+          " ",
+          "Relay",
+          "Relay_n",
+          "Button",
+          "Button_n",
+          "LED",
+          "LED_n",
+          "PWM",
+          "WifiLED",
+          "WifiLED_n",
+          "Btn_Tgl_All",
+          "Btn_Tgl_All_n",
+          "dInput",
+          "dInput_n",
+          "TglChanOnTgl",
+          "dInput_NoPullUp",
+          "dInput_NoPullUp_n",
+          "BL0937SEL",
+          "BL0937CF",
+          "BL0937CF1",
+          "ADC",
+          "SM2135DAT",
+          "SM2135CLK"
+        ]
       }
     },
     methods:{
+      onDeviceSelected(event){
+      },
       getinfo(){
         let url = window.device+'/api/info';
         fetch(url)
@@ -106,6 +171,47 @@
             }); // Never forget the final catch!
 
         console.log('would save',this.pins);
+      },
+      getRoleIndex(role){
+        return this.pinRoleNames.indexOf(role);
+      },
+      useDevice(){
+        if (this.selectedDevIndex===0){ //Nothing was selected, first item is empty
+          return;
+        }
+
+        var newPinsRoles = [];
+        var newPinsChannels = [];
+        for (let i = 0; i < this.pins.roles.length; i++){
+          newPinsRoles.push(0);
+          newPinsChannels.push(0);
+        }
+
+        var devicePins = this.devices[this.selectedDevIndex].pins;
+        for(let pin in devicePins){
+          let roleChannel = devicePins[pin].split(";");
+
+          newPinsRoles[pin]=this.getRoleIndex(roleChannel[0].trim());
+          newPinsChannels[pin] = roleChannel[1].trim();
+        }
+
+        this.pins.roles = newPinsRoles;
+        this.pins.channels = newPinsChannels;
+      },
+      getDevices(){
+        fetch("devices.json")
+          .then(response => response.json())
+          .then(data => {
+            this.devices = data.sort((a,b) => {
+              const nameA = a.name.toLowerCase();
+              const nameB = b.name.toLowerCase();
+              return nameA < nameB ? -1 : (nameA > nameB ? 1: 0);
+            });
+          })
+          .catch(err => {
+              this.error = err.toString();
+              console.error(err)
+            });
       }
     },
     mounted (){
@@ -113,6 +219,7 @@
         console.log('mounted controller');
         this.getPins();
         this.getinfo();
+        this.getDevices();
         this.interval = setInterval(()=>{
           this.getinfo();
         }, 10000);
